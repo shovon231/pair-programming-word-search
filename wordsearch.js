93aedd6ce3dcd0572c266b56d71dbdd56daee3d0
Mohammad Razaul Islam <shovon231@gmail.com>
2023-07-17 17:54:34 +0000

    pair programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (!letters.length) {
+    return false; // Handle empty letters
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true; // Found word vertically
     }
-}
+  }
+  return false;
+};
+//helper
+const transpose = function (matrix) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
 
-module.exports = wordSearch
+  const transposed = [];
+  for (let j = 0; j < numCols; j++) {
+    const newRow = [];
+    for (let i = 0; i < numRows; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    transposed.push(newRow);
+  }
+
+  return transposed;
+};
+module.exports = wordSearch;
